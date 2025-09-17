8f9a3e03fbd91059ae2ea39e53756701183f9d14
Patel Harsh <163585965+hrshh99@users.noreply.github.com>
2025-09-17 11:31:35 +0000

    image slider

---
+++ ImageSlider/index.js
@@ -0,0 +1,38 @@
+const next = document.querySelector(".fa-forward");
+
+const prev = document.querySelector(".fa-backward");
+
+const image = document.querySelectorAll(".img");
+
+const imageContainer = document.querySelector(".image-container");
+
+let currentImg = 1;
+
+let timeout;
+
+next.addEventListener("click", () => {
+  currentImg++;
+  clearTimeout(timeout);
+  updateImg();
+});
+
+prev.addEventListener("click", () => {
+  currentImg--;
+  clearTimeout(timeout);
+  updateImg();
+});
+
+updateImg();
+
+function updateImg() {
+  if (currentImg > img.length) {
+    currentImg = 1;
+  } else if (currentImg < 1) {
+    currentImg = img.length;
+  }
+  imageContainer.style.transform = `translateX(-${(currentImg - 1) * 500}px)`;
+  timeout = setTimeout(() => {
+    currentImg++;
+    updateImg();
+  }, 3000);
+}
